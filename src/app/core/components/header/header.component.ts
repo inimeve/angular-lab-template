c7476475373cf7eccddc67725b02894db24d5327
inigo <inigo.merino.vegas@gmail.com>
2019-10-28 18:57:05 +0100

    Fix: user avatar url

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -8,7 +8,7 @@ import {SidebarService} from '../sidebar/sidebar.service';
 })
 export class HeaderComponent implements OnInit {
 
-  avatarUrl: string = 'assets/avatar-set/';
+  avatarUrl: string = 'assets/images/avatar-set/';
 
   constructor(private sidebarService: SidebarService) {
     this.avatarUrl = this.avatarUrl.concat(Math.round(Math.random() * 8 + 1).toString() + '.png');
