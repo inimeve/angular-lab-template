0522f2784b7f599fe87afede84eae6d591dd95f0
inigo <inigo.merino.vegas@gmail.com>
2019-10-28 18:58:40 +0100

    Added config info to setting page

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ConfigService} from '../../core/config/config.service';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  config: any;
+
+  constructor(private configService: ConfigService) { }
 
   ngOnInit() {
+    this.config = this.configService.getConfig();
   }
 
 }
