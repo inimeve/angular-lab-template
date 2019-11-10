26849d93be523e0bc7812eccd2f8b823e5d7a569
inigo <inigo.merino.vegas@gmail.com>
2019-11-10 18:21:27 +0100

    Added config state actions

--- src/app/core/config/state/config.actions.ts
+++ src/app/core/config/state/config.actions.ts
@@ -1,5 +1,25 @@
 import {createAction} from '@ngrx/store';
 
+export const configLoadState = createAction(
+  '[Config] Load state'
+);
+
+export const configLoadStateSuccess = createAction(
+  '[Config] Load state success'
+);
+
+export const configLoadStateError = createAction(
+  '[Config] Load state error'
+);
+
+export const configLoadStoredStateSuccess = createAction(
+  '[Config] Load stored state success'
+);
+
+export const configLoadStoredStateError = createAction(
+  '[Config] Load stored state error'
+);
+
 export const configToggleSidebarState = createAction(
   '[Config] Toggle sidebar state'
 );
